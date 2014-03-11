fe348db09692eeb0f38f02742436f1b46dcbd094
Pepe Barbe <dev@antropoide.net>
2014-03-11 01:01:48 -0500

    Customize admin password when running new container

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ docker-znc
 ==========
 
 ZNC bouncer container for Docker
+
+    docker run -p 10000:6667 -p 113:113 -e ZNC_ADMIN_PASS=password elventear/znc
--- files/znc_home/run_znc.sh
+++ files/znc_home/run_znc.sh
@@ -8,15 +8,6 @@ ZNC_DATA=$HOME/.znc
 ZNC_CONFIG=$ZNC_DATA/configs/znc.conf
 ZNC_TEMPLATE=$ZNC_DATA/configs/znc.template
 
-genpass () {
-	local num_char=$1
-	test -z $num_char && num_char=15
-	local tmp=$LC_ALL
-	export LC_ALL=C
-	echo $(cat /dev/urandom | tr -dc '[:alnum:][:punct:]' | head -c${num_char})
-	export LC_ALL=$tmp
-}
-
 if [[ ! -f $ZNC_DATA/znc.pem ]]; then 
     $ZNC -d $ZNC_DATA -p
     echo Created new SSL certificate
@@ -24,14 +15,14 @@ fi
 
 if [ ! -f $ZNC_CONFIG ]; then
     if [ -z $ZNC_ADMIN_PASS ]; then
-        ZNC_ADMIN_PASS=$(genpass 20)
-        echo Auto generated Admin password $ZNC_ADMIN_PASS
-    else
-        echo Using provided password
+        echo Please provide a password for the 'admin' user via the ZNC_ADMIN_PASS enviroment variable.
+        exit 1
     fi
     ZNC_SALT="$(dd if=/dev/urandom bs=16c count=1 | md5sum | awk '{print $1}')"
-    ZNC_HASH="$(echo -n ${ZNC_PASS}${ZNC_SALT} | sha256sum | awk '{print $1}')"
+    ZNC_HASH="$(echo -n ${ZNC_ADMIN_PASS}${ZNC_SALT} | sha256sum | awk '{print $1}')"
     sed -e "s/@hash@/$ZNC_HASH/g" -e "s/@salt@/$ZNC_SALT/g" $ZNC_TEMPLATE > $ZNC_CONFIG
 fi 
+
+unset $ZNC_ADMIN_PASS
 
 exec $ZNC -f -d $ZNC_DATA
